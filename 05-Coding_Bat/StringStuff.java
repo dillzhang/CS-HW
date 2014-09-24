5ebf04fa8ab72aaae40d11b771b3ce5c9e622b30
Dillon Zhang <d1998z@gmail.com>
2014-09-24 09:15:26 -0400

    Commented Working

--- 05-Coding_Bat/StringStuff.java
+++ 05-Coding_Bat/StringStuff.java
@@ -2,16 +2,19 @@
 public class StringStuff {
 
 //Nonstart
+/*Working Version*/
 public String nonStart(String a, String b) {
     return a.substring(1)+b.substring(1);
 }
 
 //makeABBA
+/*Working Version*/
 public String makeABBA(String a, String b) {
     return a+b+b+a;
 }
 
 //diff21
+/*Working Version*/
 public int diff21(int n) {
     if (n>21) 
 	{return 2*n-42;}
@@ -20,11 +23,13 @@ public int diff21(int n) {
 }
 
 //nearHundred
+/*Working Version*/
 public boolean nearHundred(int n) {
     return Math.abs(100-n) <= 10 | Math.abs(200-n) <= 10;
 }
 
 //mixstart
+/*Working Version*/
 public boolean mixStart(String str) {
     if (str.length() >= 3) 
         {return "ix".equals(str.substring(1,3));}
@@ -33,6 +38,7 @@ public boolean mixStart(String str) {
 }
 
 //teaparty
+/*Working Version*/
 public int teaParty(int tea, int candy) {
     if (tea >= 5 && candy >= 5)
         if (tea/candy >= 2 || candy/tea>=2)
@@ -45,6 +51,7 @@ public int teaParty(int tea, int candy) {
 
 
 //lastDigit
+/*Working Version*/
 public boolean lastDigit(int a, int b, int c) {
     return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
 }
