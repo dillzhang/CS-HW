d01c6230ec9c411638b4fa28232bc082e085ee58
Dillon Zhang <d1998z@gmail.com>
2014-10-03 21:05:51 -0400

    Finished and Commented

---
+++ 08-Two_More_Bats/Driver.java
@@ -0,0 +1,28 @@
+public class Driver {
+    public static void main(String[] args) {
+	
+	//Intializes a new class variable
+	Bats b = new Bats();
+
+	//Test for stringSplosion
+	System.out.println("stringSplosion('code')");
+	System.out.println(b.stringSplosion("code"));
+	System.out.println("stringSplosion('abcd')");
+	System.out.println(b.stringSplosion("abcd"));
+	System.out.println("stringSplosion('testing123')");
+	System.out.println(b.stringSplosion("testing123"));
+	System.out.println("stringSplosion('xyz')");
+	System.out.println(b.stringSplosion("xyz"));
+        
+    //Test for stringX
+    System.out.println("stringx('texting')");
+    System.out.println(b.stringX("texting"));
+    System.out.println("stringx('xhellox')");
+    System.out.println(b.stringX("xhellox"));
+    System.out.println("stringx('xxhelloxworldxx')");
+    System.out.println(b.stringX("xxhelloxworldxx"));
+    System.out.println("stringx('abcxyz')");
+    System.out.println(b.stringX("abcxyz"));
+    
+    }
+}
