04427fef3e36d2afd902e76002a92833e59ebb95
Dillon Zhang <d1998z@gmail.com>
2014-11-08 10:15:53 -0500

    Completed Homework 15

---
+++ 15-Sarray/Driver.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	Sarray alpha = new Sarray();
+	
+	for (int counter = 0; counter < 10; counter++) {
+	    alpha.add(counter);
+	}
+	System.out.println("Adding completed");
+	System.out.println("Sarray: " + Arrays.toString(alpha.data));
+	System.out.println("Last: " + alpha.last);
+	
+	System.out.println();
+	alpha.add(2,11);
+	System.out.println("Index Adding completed");
+	System.out.println("Sarray: " + Arrays.toString(alpha.data));
+	System.out.println("Last: " + alpha.last);
+	
+	System.out.println();
+	System.out.println(Arrays.toString(alpha.data));
+	System.out.println("Size: " + alpha.size());
+	System.out.println("alpha[4]: " + alpha.get(4));
+
+	System.out.println();
+	System.out.println("Setting index 7 to 100: " + alpha.set(7,100));
+	System.out.println("After Setting: " + Arrays.toString(alpha.data));
+	
+	System.out.println();
+	System.out.println("Removing index 4: " + alpha.remove(4));
+	System.out.println("After Removal: " + Arrays.toString(alpha.data));
+    }
+}
---
+++ 15-Sarray/Sarray.java
@@ -0,0 +1,116 @@
+public class Sarray {
+    int[] data;
+    int   last;
+    /*
+    public Sarray(int size, int[] seeds) {
+	data = new int[size];
+	last = 0;
+	for (int counter = 0; counter < data.length && counter<seeds.length; counter++) {
+	    data[counter] = seeds[counter];
+	    last += 1;
+	}
+    }
+    
+    public Sarray(int[] seeds, int size) {
+	Sarray(size, seeds);
+    }
+
+    public Sarray(int size) {
+	Sarray(size,[0]); 
+    }
+    
+    public Sarray(int[] seeds) {
+	Sarray(seeds.length, seeds);
+    }
+    */
+    public Sarray() {
+        // set up the initial instance variables
+	data = new int[1];
+	last = 0;
+	//Sarray(1,[0]);
+    }
+
+    private void grow() {
+	int[] buffer = new int[data.length+1];
+	for (int counter = 0; counter < data.length; counter++) {
+	    buffer[counter] = data[counter];
+	}
+	data = buffer;
+    }
+
+    public boolean add(int i){
+        // adds an item to the end of the list, grow if needed
+        // returns true
+	if (last < data.length) {
+	    data[last] = i;
+	    last += 1;
+	} else {
+	    grow();
+	    add(i);
+	}
+
+	return true;
+    }
+
+    
+    public void  add(int index, int i){
+        // adds item i  at index, shifting everything down as needed.
+        // also grows as needed 
+	if (last < data.length) {
+	    for(int counter = last; counter > index; counter--) {
+		data[counter] = data[counter-1];
+	    }
+	    data[index] = i;
+	    last += 1;
+	} else {
+	    grow();
+	    add(index, i);
+	}
+    }
+
+    
+    public int size() {
+        // returns the number of items in the list (not the array size)
+	return last;
+    }
+    
+    public int get(int index) {
+        // returns the item at location index of the lsit
+	if (index < last && index >= 0) {
+	    return data[index];
+	} else {
+	    return -1;
+	}
+    }
+
+    public int set(int index, int i){
+        // sets the item at location index to value i
+        // returns the old value.
+	if (index < last && index >= 0) {
+	    int buffer = data[index];
+	    data[index] = i;
+	    return buffer;
+	} else {
+	    return -1;
+	}
+    }
+
+    public int remove(int index){
+        // removes the item at index i
+        // returns the old value
+	if (index < last && index >= 0){
+	    int buffer = data[index];
+	    last -= 1;
+	    for(int counter = index; counter < data.length; counter ++) {
+		if (counter < last) {
+		    data[counter] = data[counter + 1];
+		} else {
+		    data[counter] = 0;
+		}
+	    }
+	    return buffer;
+	} else {
+	    return -1;
+	}
+    }
+}
