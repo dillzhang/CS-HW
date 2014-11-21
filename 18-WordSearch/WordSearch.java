766fcb044ba01fcd3a35585ad1b5d0b2d059f076
Dillon Zhang <d1998z@gmail.com>
2014-11-21 09:12:14 -0500

    fillBoard()

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -1,8 +1,12 @@
+import java.util.Random;
+
 public class WordSearch {
     
     //===========================================================================================================================================================================
     
     //INSTANCE VARIABLES
+    Random rand;
+    
     private char[][] board;
 
     private int[][] directions = { //Used in word placement direction
@@ -20,6 +24,7 @@ public class WordSearch {
 
     //CONSTRUCTORS
     public WordSearch(int r, int c){
+	rand = new Random();
 	board = new char[r][c];
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
@@ -50,28 +55,25 @@ public class WordSearch {
 
     //METHODS
     
-    //addWord( desired word, starting row, starting column, desired directon ) 
-    //--> Direction calculations are stored in the private array direction
-    //--> --> 0 - Right Horizontal, each one after is a 45 degree counterclockwise, till 7 - Right Downward
-    //first runs through to check if the word can be legally added to the selected location at the selected orientation
-    //second run through to place the word in the desired location after scouting of location reveals no problem
-    //if error presents itself, code terminates and prints "Word does not fit location"
-    public void addWord(String w, int row, int col, int d){
-	String tester = w;
+    public boolean addWordTester(String w, int row, int col, int d){
 	int r = row;
 	int c = col;
+	
 	for (int i = 0; i<w.length(); i++) {
-	    if (r>=0 && r<board[0].length && c>=0 && c<board.length && (board[r][c] == tester.charAt(i) || board[r][c] =='.')) {
+	    if (r>=0 && r<board[0].length && c>=0 && c<board.length && (board[r][c] == w.charAt(i) || board[r][c] =='.')) {
 		r += directions[d][0];
 		c += directions[d][1];
 	    } else {
-		System.out.println("Word does not fit location");
-		System.exit(0);
+		return false;
 	    }
 	}
 	
-	r = row;
-	c = col;
+	return true;
+    }
+
+    public void addWordHelper(String w, int row, int col, int d){
+	int r = row;
+	int c = col;
 	
 	for (int i = 0; i<w.length(); i++) {
 	    board[r][c] = w.charAt(i);
@@ -80,20 +82,52 @@ public class WordSearch {
 	}
     }
 
+    public void addWord(String w){
+	boolean failure = true;
+	
+	while (failure) {
+	    int tryRow = rand.nextInt(board.length);
+	    int tryCol = rand.nextInt(board[0].length);
+	    int tryDir = rand.nextInt(8);
+
+	    if (addWordTester(w,tryRow,tryCol,tryDir)) {
+		addWordHelper(w,tryRow,tryCol,tryDir);
+		failure = false;
+	    }
+	}
+    }
+	
+    public void fillBoard(){
+	for(int i=0;i<board.length;i++){
+	    for(int j = 0; j<board[i].length; j++) {
+		if (board[i][j] == '.') {
+		    board[i][j] = "abcdefghijklmnopqrstuvwxyz".charAt(rand.nextInt(26));
+		}
+	    }
+	}
+    }
+    
     //===========================================================================================================================================================================
 
     //MAIN METHOD
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
+	
 	System.out.println(w);
-	w.addWord("hello",3,15,0); // should work
+	
+	w.addWord("hello"); // should work
 	System.out.println(w);
+	
 	//w.addWord("look",3,14,0); // test illegal overlap
 	//w.addWord("look",3,18,0); // test legal overlap
 	//w.addWord("look",-3,20,0); // test illegal row
 	//w.addWord("look",3,55,0); // test illegal col
-	w.addWord("look",3,18,2); //test vertical placement
-	w.addWord("alphabet",4,19,2);//testing vertical placement
+	
+	w.addWord("look"); //test vertical placement
+	w.addWord("alphabet");//testing vertical placement
+	System.out.println(w);
+	
+	w.fillBoard();
 	System.out.println(w);
     }
 }
