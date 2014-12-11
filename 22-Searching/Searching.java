cbc482b2613edf9cb57fb7bd1885fd6726353fd7
Dillon Zhang <d1998z@gmail.com>
2014-12-11 18:39:14 -0500

    Finished Searching Methods

---
+++ 22-Searching/Searching.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+public class Searching {
+    private Comparable[] a;
+    private int last = 0;
+    private Random rand = new Random();
+    
+    public Searching(int i){
+	a = new Comparable[i];
+    }
+
+    public Searching(){
+	this(10);
+    }
+    
+    public String toString() {
+	return Arrays.toString(a);
+    }
+    
+    public void additem(Comparable item) {
+	if (last < a.length) {
+	    a[last] = item;
+	} else {
+	    throw new IndexOutOfBoundsException();
+	}
+    }
+    
+    public void randFill() {
+	for (int i = 0; i < a.length; i++) {
+	    a[i] = rand.nextInt(100);
+	}
+	last = a.length;
+    }
+
+    public void sorter() {
+	Arrays.sort(a);
+	}
+
+    
+    public Comparable isearch(Comparable item) {
+        for(int counter = 0; counter<a.length; counter++) {
+            if (a[counter].equals(item)) {
+                return counter;
+            }
+        }
+        return null;
+    }
+
+    public Comparable bsearch(Comparable item) {
+        int start = 0, stop = a.length - 1;
+
+        while (start < stop) {
+
+	    int middle = (start + stop) / 2;
+ 
+            if (a[middle].equals(item)){
+                return middle;
+            } else if (a[middle].compareTo(item) < 0) {
+                start = middle + 1;
+            } else {
+                stop = middle -1;
+            }
+	    
+        }
+        return null;
+    }
+
+    public Comparable rbsearch(Comparable item) {
+	return rbsearch(item, 0, a.length-1);
+    }
+
+    public Comparable rbsearch(Comparable item, int start, int stop) {
+	int middle = (start + stop) / 2;
+	if (stop <= start) {
+	    return null;
+	}else if (a[middle].equals(item)) {
+	    return middle;
+	} else if (a[middle].compareTo(item) < 0) {
+	    return rbsearch(item, middle + 1, stop);
+	} else {
+	    return rbsearch(item, start, middle -1);
+	}
+    }
+
+    public static void main(String[] args) {
+	Random rand = new Random();
+	Searching searcher = new Searching(100);
+	int alpha = rand.nextInt(10);
+	
+	System.out.println(alpha);
+       
+	searcher.randFill();
+
+	System.out.println(searcher);
+
+	System.out.println(searcher.isearch(alpha));
+
+	searcher.sorter();
+	System.out.println(searcher);
+	System.out.println(searcher.bsearch(alpha));
+	System.out.println(searcher.rbsearch(alpha));
+    }
+    
+}
